dd626118aa05bfc32c5e6088235936b4d4735356
NilushaD98 <shannilusharr456@gmail.com>
2023-05-26 23:44:05 +0530

    develop user authentication services for authenticate user

--- src/main/java/com/example/OlympicDatabase/OlympicDatabaseApplication.java
+++ src/main/java/com/example/OlympicDatabase/OlympicDatabaseApplication.java
@@ -20,30 +20,26 @@ public class OlympicDatabaseApplication {
 		SpringApplication.run(OlympicDatabaseApplication.class, args);
 	}
 
-	@Bean
-	PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
-	@Bean
-	CommandLineRunner run(UserService userService){
-		return args -> {
-			userService.saveRole(new Role(null,"ROLE_USER"));
-			userService.saveRole(new Role(null,"ROLE_MANAGER"));
-			userService.saveRole(new Role(null,"ROLE_ADMIN"));
-			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
-
-			userService.saveUser(new User(null,"Nilusha Dissanayaka","NilushaD","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Heshan Thathsara","HeshanT","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Keshan Lahiru","KeshanL","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Fahim Fa","FahimF","1234",new ArrayList<>()));
-
-			userService.addRoleToUser("NilushaD","ROLE_USER");
-			userService.addRoleToUser("HeshanT","ROLE_MANAGER");
-			userService.addRoleToUser("KeshanL","ROLE_ADMIN");
-			userService.addRoleToUser("FahimF","ROLE_SUPER_ADMIN");
-			userService.addRoleToUser("FahimF","ROLE_ADMIN");
-			userService.addRoleToUser("FahimF","ROLE_USER");
-		};
-	}
+//	@Bean
+//	CommandLineRunner run(UserService userService){
+//		return args -> {
+//			userService.saveRole(new Role(null,"ROLE_USER"));
+//			userService.saveRole(new Role(null,"ROLE_MANAGER"));
+//			userService.saveRole(new Role(null,"ROLE_ADMIN"));
+//			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+//
+//			userService.saveUser(new User(null,"Nilusha Dissanayaka","NilushaD","1234",new ArrayList<>()));
+//			userService.saveUser(new User(null,"Heshan Thathsara","HeshanT","1234",new ArrayList<>()));
+//			userService.saveUser(new User(null,"Keshan Lahiru","KeshanL","1234",new ArrayList<>()));
+//			userService.saveUser(new User(null,"Fahim Fa","FahimF","1234",new ArrayList<>()));
+//
+//			userService.addRoleToUser("NilushaD","ROLE_USER");
+//			userService.addRoleToUser("HeshanT","ROLE_MANAGER");
+//			userService.addRoleToUser("KeshanL","ROLE_ADMIN");
+//			userService.addRoleToUser("FahimF","ROLE_SUPER_ADMIN");
+//			userService.addRoleToUser("FahimF","ROLE_ADMIN");
+//			userService.addRoleToUser("FahimF","ROLE_USER");
+//		};
+//	}
 
 }
--- src/main/java/com/example/OlympicDatabase/security/SecurityConfig.java
+++ src/main/java/com/example/OlympicDatabase/security/SecurityConfig.java
@@ -1,28 +1,50 @@
 package com.example.OlympicDatabase.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.
+                csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().
+                authorizeRequests().anyRequest().permitAll().
+                and().
+                addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
